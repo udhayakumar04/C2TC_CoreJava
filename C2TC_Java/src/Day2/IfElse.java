aa73e60af051028de6554b29cacb80295a6d9b37
udhayakumar04 <130219032+udhayakumar04@users.noreply.github.com>
2025-07-21 08:05:04 -0700

    If Else

---
+++ C2TC_Java/src/Day2/IfElse.java
@@ -0,0 +1,13 @@
+package Day2;
+
+public class IfElse {
+	public static void main(String[] args) {
+		int mark=35;
+		
+		if(mark>=35)
+			System.out.println("your pass");	
+		else
+			System.out.println("your fail");
+
+}
+}
