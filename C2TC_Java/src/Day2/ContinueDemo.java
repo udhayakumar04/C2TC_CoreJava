e9e666f95025c57729638e504b6c2857b6f35da0
udhayakumar04 <130219032+udhayakumar04@users.noreply.github.com>
2025-07-21 08:36:50 -0700

    Switch Case

---
+++ C2TC_Java/src/Day2/ContinueDemo.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class ContinueDemo {
+	public static void main(String[] args) {
+		for (int i = 5; i < 15; i++)
+		{
+		if (i%2 != 0)
+		continue;
+		System.out.println(i + " ");
+		}
+	}
+}
