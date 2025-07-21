23fee906885f2a9cdd79e1b693d071a86612dcbf
udhayakumar04 <130219032+udhayakumar04@users.noreply.github.com>
2025-07-21 04:02:25 -0700

    Identifiers

---
+++ C2TC_Java/src/Day1/Identifier.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Identifier {
+	public static void main(String[] args){
+	int $number = 10;
+	System.out.println("value of the number variable is : "+ $number);
+
+	String studentName="Udhaya"; 
+	System.out.println("value is : "+ studentName);
+}
+}
