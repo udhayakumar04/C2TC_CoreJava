ba5a98570684ec17d9d9ad267593c00cf375b3af
udhayakumar04 <130219032+udhayakumar04@users.noreply.github.com>
2025-07-21 08:08:02 -0700

    For Loop

---
+++ C2TC_Java/src/Day2/ForLoop.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class ForLoop {
+	public static void main(String[] args) 
+	{	
+		for(int i = 1; i<=10 ; i++)   
+		{	
+			System.out.print("Value of i: ");
+			System.out.println(i);
+}
+	}
+}
