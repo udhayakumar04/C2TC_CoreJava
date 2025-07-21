d3d1106f83ae98ec2b72fec3c5c489d6ddb712b1
udhayakumar04 <130219032+udhayakumar04@users.noreply.github.com>
2025-07-21 08:10:40 -0700

    If Demo

---
+++ C2TC_Java/src/Day2/IfDemo.java
@@ -0,0 +1,11 @@
+package Day2;
+
+public class IfDemo {
+	public static void main(String[] args) {
+
+		int x = 25;
+		if (x != 25)
+			System.out.println("Value of x is not 25");
+		System.out.println("Value of x is 25");
+}
+}
