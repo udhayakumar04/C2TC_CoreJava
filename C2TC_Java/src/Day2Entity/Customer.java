9b9c6441b95088d4720156826e06d32148e8a43e
udhayakumar04 <130219032+udhayakumar04@users.noreply.github.com>
2025-07-22 04:06:14 -0700

    Entity

---
+++ C2TC_Java/src/Day2Entity/Customer.java
@@ -0,0 +1,31 @@
+package Day2Entity;
+
+public class Customer {
+	private int cid;
+	private String cname;
+	private String city;
+	
+	public int getCid() {
+		return cid;
+}
+public void setCid(int cid) {
+	this.cid = cid;
+}
+public String getCname() {
+	return cname;
+}
+public void setCname(String cname) {
+	this.cname = cname;
+}
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+@Override
+public String toString() {
+	return "Customer cid=" + cid + ", cname=" + cname + ", city=" + city + ",";
+}
+
+}
---
+++ C2TC_Java/src/Day2Entity/CustomerDemo.java
@@ -0,0 +1,17 @@
+package Day2Entity;
+
+public class CustomerDemo {
+	public static void main(String[] args) {
+	Customer c1=new Customer();
+	c1.setCid(101);
+	c1.setCname("udhaya");
+	c1.setCity("Cuddalore");
+	System.out.println(c1);
+	
+	Customer c2 = new Customer();
+	c2.setCid(101);
+	c2.setCname("udhaya");
+	c2.setCity("Cuddalore");
+	System.out.println(c2);
+	}
+}
