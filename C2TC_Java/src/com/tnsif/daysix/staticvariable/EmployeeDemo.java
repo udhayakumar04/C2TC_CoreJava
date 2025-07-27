2967f7cbf26252bdd23b2aa66a75b6c17676f539
udhayakumar04 <130219032+udhayakumar04@users.noreply.github.com>
2025-07-27 01:12:32 -0700

    Day6

---
+++ C2TC_Java/src/com/tnsif/daysix/staticvariable/EmployeeDemo.java
@@ -0,0 +1,19 @@
+
+//Program to demonstrate static variables
+package com.tnsif.daysix.staticvariable;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		// Create the first object of the class and pass the two arguments with type
+		// string and int.
+		System.out.println(Employee.companyName);
+		Employee e = new Employee("udhaya", 51);
+		System.out.println(e);
+		
+		// Similarly, create the second object of the class and pass the two arguments.
+		e = new Employee("Kumar", 52);
+		System.out.println(e);
+	}
+}
+
