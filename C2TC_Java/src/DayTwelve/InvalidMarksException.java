55f09a58f71dca2ec5526d3794ba49a268a3c420
udhayakumar04 <130219032+udhayakumar04@users.noreply.github.com>
2025-09-02 09:35:58 -0700

    Core Java

---
+++ C2TC_Java/src/DayTwelve/InvalidMarksException.java
@@ -0,0 +1,13 @@
+package DayTwelve;
+
+public class InvalidMarksException extends Exception{
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+}
---
+++ C2TC_Java/src/DayTwelve/UncheckedException.java
@@ -0,0 +1,13 @@
+package DayTwelve;
+
+public class UncheckedException {
+	public static void main(String[] args) {
+		int intArray[];
+		//try {
+			intArray = new int[] { 1, 2, 3, 4 };
+			System.out.println(intArray[6]);
+		//} catch (ArrayIndexOutOfBoundsException e) {
+			//System.err.println("Specified index does not exist. "+e.getMessage());
+		//}
+	}
+}
